8af3e56eb81d070cc4278efd5205c706c0eab1f3
ChanceXML <joseguest1337@gmail.com>
2026-03-06 21:10:44 -0700

    Update Hitbox.hx

--- source/mobile/controls/Hitbox.hx
+++ source/mobile/controls/Hitbox.hx
@@ -4,7 +4,7 @@ import flixel.FlxG;
 import flixel.group.FlxSpriteGroup;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
-import mobile.backend.TouchInput;
+import mobile.controls.TouchInput;
 import funkin.game.PlayState;
 
 class Hitbox extends FlxSpriteGroup {
