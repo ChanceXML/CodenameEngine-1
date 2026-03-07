fd3acbca4756e0ea638ba2a39eb05563a4d64cfc
ChanceXML <joseguest1337@gmail.com>
2026-03-06 20:03:16 -0700

    Update Hitbox.hx

--- source/mobile/controls/Hitbox.hx
+++ source/mobile/controls/Hitbox.hx
@@ -1,105 +1,46 @@
-package mobile.controls;
+package funkin.mobile.controls;
 
-#if android
 import flixel.FlxG;
-import flixel.group.FlxGroup;
 import flixel.FlxSprite;
-import flixel.math.FlxPoint;
-import funkin.backend.TurboControls;
-import funkin.backend.system.Controls;
-import funkin.options.PlayerSettings;
+import flixel.group.FlxSpriteGroup;
+import flixel.util.FlxColor;
+import funkin.mobile.backend.TouchInput;
 import funkin.game.PlayState;
 
-class Hitbox extends FlxGroup
-{
-    var hintBG:FlxSprite;
+class HitBox extends FlxSpriteGroup {
 
-    var leftRect:FlxSprite;
-    var downRect:FlxSprite;
-    var upRect:FlxSprite;
-    var rightRect:FlxSprite;
+    public var hitboxHint:FlxSprite;
+    public var hitboxes:Array<FlxSprite> = [];
 
-    public var leftTurbo:TurboControls;
-    public var downTurbo:TurboControls;
-    public var upTurbo:TurboControls;
-    public var rightTurbo:TurboControls;
-
-    public function new()
-    {
+    public function new() {
         super();
 
-        var w = FlxG.width / 4;
-        var h = FlxG.height;
+        hitboxHint = new FlxSprite(FlxG.width / 2 - 100, FlxG.height - 150);
+        hitboxHint.loadGraphic("assets/images/hitbox_hint.png");
+        add(hitboxHint);
 
-        hintBG = new FlxSprite(0, 0, "assets/images/mobile/hitbox_hint.png");
-        hintBG.setGraphicSize(FlxG.width, FlxG.height);
-        add(hintBG);
-
-        leftRect  = makeRect(0, 0, w, h, 0x80FF0000);
-        downRect  = makeRect(w, 0, w, h, 0x8000FFFF);
-        upRect    = makeRect(w*2, 0, w, h, 0x8000FF00);
-        rightRect = makeRect(w*3, 0, w, h, 0x80FFFF00);
-
-        add(leftRect);
-        add(downRect);
-        add(upRect);
-        add(rightRect);
-
-        leftTurbo  = new TurboControls([PlayerSettings.solo.controls.getActionFromControl(Control.LEFT)]);
-        downTurbo  = new TurboControls([PlayerSettings.solo.controls.getActionFromControl(Control.DOWN)]);
-        upTurbo    = new TurboControls([PlayerSettings.solo.controls.getActionFromControl(Control.UP)]);
-        rightTurbo = new TurboControls([PlayerSettings.solo.controls.getActionFromControl(Control.RIGHT)]);
+        var width:Float = FlxG.width / 4;
+        var height:Float = FlxG.height;
+        for (i in 0...4) {
+            var hb = new FlxSprite(i * width, 0, width, height);
+            hb.makeGraphic(width, height, FlxColor.WHITE);
+            hb.alpha = 0;
+            hitboxes.push(hb);
+            add(hb);
+        }
     }
 
-    function makeRect(x:Float, y:Float, w:Float, h:Float, color:Int):FlxSprite
-    {
-        var s = new FlxSprite(x, y);
-        s.makeGraphic(Std.int(w), Std.int(h), color);
-        s.alpha = 0.2;
-        s.immovable = true;
-        s.moves = false;
-        return s;
-    }
-
-    override function update(elapsed:Float)
-    {
+    override public function update(elapsed:Float):Void {
         super.update(elapsed);
 
-        leftRect.alpha = 0.2;
-        downRect.alpha = 0.2;
-        upRect.alpha = 0.2;
-        rightRect.alpha = 0.2;
-
-        for (touch in FlxG.touches.list)
-        {
-            if (!touch.pressed) continue;
-
-            var pos:FlxPoint = touch.getScreenPosition();
-
-            if (leftRect.overlapsPoint(pos))
-            {
-                leftTurbo.update(elapsed);
-                if (leftTurbo.activated) PlayState.instance.triggerNoteSafe(0);
-            }
-
-            if (downRect.overlapsPoint(pos))
-            {
-                downTurbo.update(elapsed);
-                if (downTurbo.activated) PlayState.instance.triggerNoteSafe(1);
-            }
-
-            if (upRect.overlapsPoint(pos))
-            {
-                upTurbo.update(elapsed);
-                if (upTurbo.activated) PlayState.instance.triggerNoteSafe(2);
-            }
-
-            if (rightRect.overlapsPoint(pos))
-            {
-                rightTurbo.update(elapsed);
-                if (rightTurbo.activated) PlayState.instance.triggerNoteSafe(3);
+        for (i in 0...hitboxes.length) {
+            var hb = hitboxes[i];
+            if (TouchInput.pressed(hb)) {
+                hb.alpha = 0.25;
+                if (PlayState.instance != null) PlayState.instance.hitNote(i);
+            } else {
+                hb.alpha = 0;
             }
         }
     }
 }
-#end
