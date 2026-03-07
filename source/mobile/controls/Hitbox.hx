d1cf7fc15081e75e11ccd9476d1a40c513fa0413
ChanceXML <joseguest1337@gmail.com>
2026-03-06 20:37:47 -0700

    Update Hitbox.hx

--- source/mobile/controls/Hitbox.hx
+++ source/mobile/controls/Hitbox.hx
@@ -14,16 +14,14 @@ class Hitbox extends FlxSpriteGroup {
     public function new(hintPath:String, width:Float, height:Float) {
         super();
 
-        // Create static hint
         hint = new FlxSprite(0, 0, hintPath);
         hint.scrollFactor.set();
         add(hint);
 
-        // Create 4 hitboxes for notes (you can expand if needed)
         for (i in 0...4) {
             var hb = new FlxSprite(i * width, 0);
             hb.makeGraphic(width, height, FlxColor.WHITE);
-            hb.alpha = 0; // invisible by default
+            hb.alpha = 0; 
             hb.scrollFactor.set();
             add(hb);
             notes.push(hb);
