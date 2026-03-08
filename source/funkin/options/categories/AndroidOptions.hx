840f00bd78f9ad5c1cace37eca9833c65576babb
ChanceXML <joseguest1337@gmail.com>
2026-03-08 11:30:19 -0700

    Update AndroidOptions.hx

--- source/funkin/options/categories/AndroidOptions.hx
+++ source/funkin/options/categories/AndroidOptions.hx
@@ -2,7 +2,7 @@ package funkin.options.categories;
 
 class AndroidOptions extends TreeMenuScreen {
 	public function new() {
-		super('optionsTree.android-name', 'optionsTree.android-desc', 'AndroidOptions.');
+		super('optionsTree.android-name', 'optionsTree.android-desc', 'AndroidOptions');
 
 		add(new Checkbox(getNameID('hitboxHints'), getDescID('hitboxHints'), 'hitboxHints'));
 		add(new SliderOption(getNameID('hitboxOpacity'), getDescID('hitboxOpacity'), 0, 1, 0.05, 5, 'hitboxOpacity'));
