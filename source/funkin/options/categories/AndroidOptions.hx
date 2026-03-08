19eb0b0562231799a228755fbb9fc91ff72a90b7
ChanceXML <joseguest1337@gmail.com>
2026-03-08 12:01:20 -0700

    Update AndroidOptions.hx

--- source/funkin/options/categories/AndroidOptions.hx
+++ source/funkin/options/categories/AndroidOptions.hx
@@ -1,10 +1,29 @@
 package funkin.options.categories;
 
-class AndroidOptions extends TreeMenuScreen {
-	public function new() {
-		super('optionsTree.android-name', 'optionsTree.android-desc', 'AndroidOptions');
+import flixel.FlxG;
+import funkin.options.Options;
+import funkin.ui.menu.Checkbox;
+import funkin.ui.menu.SliderOption;
 
-		add(new Checkbox(getNameID('hitboxHints'), getDescID('hitboxHints'), 'hitboxHints'));
-		add(new SliderOption(getNameID('hitboxOpacity'), getDescID('hitboxOpacity'), 0, 1, 0.05, 5, 'hitboxOpacity'));
-	}
+class AndroidOptions extends TreeMenuScreen {
+
+    public function new() {
+        super('optionsTree.android-name', 'optionsTree.android-desc', '');
+
+        add(new Checkbox(
+            getNameID('hitboxHints'),      // Display name
+            getDescID('hitboxHints'),      // Description
+            'hitboxHints'                  // Save key
+        ));
+
+        add(new SliderOption(
+            getNameID('hitboxOpacity'),    // Display name
+            getDescID('hitboxOpacity'),    // Description
+            0,                             // Min value
+            1,                             // Max value
+            0.05,                          // Step
+            5,                             // Precision
+            'hitboxOpacity'                // Save key
+        ));
+    }
 }
