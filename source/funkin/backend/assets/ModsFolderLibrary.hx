fd291b455b929f4fa6dde6769cd78ebda32618bd
ChanceXML <joseguest1337@gmail.com>
2026-03-08 08:14:01 -0700

    Update ModsFolderLibrary.hx

--- source/funkin/backend/assets/ModsFolderLibrary.hx
+++ source/funkin/backend/assets/ModsFolderLibrary.hx
@@ -5,6 +5,7 @@ import lime.media.AudioBuffer;
 import lime.graphics.Image;
 import lime.text.Font;
 import lime.utils.Bytes;
+import haxe.io.Path;
 
 #if MOD_SUPPORT
 import sys.FileStat;
@@ -17,15 +18,14 @@ class ModsFolderLibrary extends AssetLibrary implements IModsAssetLibrary {
 	public var basePath:String;
 	public var modName:String;
 	public var libName:String;
-	//public var useImageCache:Bool = true;
 	public var prefix = 'assets/';
 
 	public function new(basePath:String, libName:String, ?modName:String) {
-	this.basePath = basePath;
-	this.libName = libName;
-	this.prefix = 'assets/';
- 	this.modName = modName == null ? libName : modName;
-	super();
+		this.basePath = basePath;
+		this.libName = libName;
+		this.prefix = 'assets/';
+		this.modName = modName == null ? libName : modName;
+		super();
 	}
 
 	function toString():String {
@@ -41,40 +41,31 @@ class ModsFolderLibrary extends AssetLibrary implements IModsAssetLibrary {
 	}
 
 	public override function getAudioBuffer(id:String):AudioBuffer {
-		if (!exists(id, "SOUND"))
-			return null;
+		if (!exists(id, "SOUND")) return null;
 		var path = getAssetPath();
 		editedTimes[id] = FileSystem.stat(path).mtime.getTime();
-		var e = AudioBuffer.fromFile(path);
-		// LimeAssets.cache.audio.set('$libName:$id', e);
-		return e;
+		return AudioBuffer.fromFile(path);
 	}
 
 	public override function getBytes(id:String):Bytes {
-		if (!exists(id, "BINARY"))
-			return null;
+		if (!exists(id, "BINARY")) return null;
 		var path = getAssetPath();
 		editedTimes[id] = FileSystem.stat(path).mtime.getTime();
-		var e = Bytes.fromFile(path);
-		return e;
+		return Bytes.fromFile(path);
 	}
 
 	public override function getFont(id:String):Font {
-		if (!exists(id, "FONT"))
-			return null;
+		if (!exists(id, "FONT")) return null;
 		var path = getAssetPath();
 		editedTimes[id] = FileSystem.stat(path).mtime.getTime();
 		return ModsFolder.registerFont(Font.fromFile(path));
 	}
 
 	public override function getImage(id:String):Image {
-		if (!exists(id, "IMAGE"))
-			return null;
+		if (!exists(id, "IMAGE")) return null;
 		var path = getAssetPath();
 		editedTimes[id] = FileSystem.stat(path).mtime.getTime();
-
-		var e = Image.fromFile(path);
-		return e;
+		return Image.fromFile(path);
 	}
 
 	public override function getPath(id:String):String {
@@ -95,12 +86,10 @@ class ModsFolderLibrary extends AssetLibrary implements IModsAssetLibrary {
 		try {
 			var result:Array<String> = [];
 			for(e in FileSystem.readDirectory(path))
-				if (FileSystem.isDirectory('$path$e') == folders)
+				if (FileSystem.isDirectory(Path.join([path, e])) == folders)
 					result.push(e);
 			return result;
-		} catch(e) {
-			// woops!!
-		}
+		} catch(e) {}
 		return [];
 	}
 
@@ -109,39 +98,29 @@ class ModsFolderLibrary extends AssetLibrary implements IModsAssetLibrary {
 		return FileSystem.exists(getAssetPath());
 	}
 
-
 	private function __isCacheValid(cache:Map<String, Dynamic>, asset:String, isLocalCache:Bool = false) {
-		if (!editedTimes.exists(asset))
-			return false;
+		if (!editedTimes.exists(asset)) return false;
 		var editedTime = editedTimes[asset];
-		if (editedTime == null || editedTime < FileSystem.stat(getPath(asset)).mtime.getTime()) {
-			// destroy already existing to prevent memory leak!!!
-			/*var asset = cache[asset];
-			if (asset != null) {
-				switch(Type.getClass(asset)) {
-					case lime.graphics.Image:
-						trace("getting rid of image cause replaced");
-						cast(asset, lime.graphics.Image);
-				}
-			}*/
-			return false;
-		}
-
+		if (editedTime == null || editedTime < FileSystem.stat(getPath(asset)).mtime.getTime()) return false;
 		if (!isLocalCache) asset = '$libName:$asset';
-
 		return cache.exists(asset) && cache[asset] != null;
 	}
 
+	// FIX: Re-added file.file to prevent missing filenames like ".png"
 	private function __parseAsset(asset:String):Bool {
-		if (!asset.startsWith(prefix)) return false;
+		if (asset == null || !asset.startsWith(prefix)) return false;
+		
 		_parsedAsset = asset.substr(prefix.length);
+		
 		if(ModsFolder.useLibFile) {
-			var file = new haxe.io.Path(_parsedAsset);
+			var file = new Path(_parsedAsset);
 			if(file.file.startsWith("LIB_")) {
 				var library = file.file.substr(4);
 				if(library != modName) return false;
-
-				_parsedAsset = file.dir + "." + file.ext;
+				
+				// Reconstruct path including the directory and the actual filename
+				var dir = (file.dir != null && file.dir != "") ? file.dir + "/" : "";
+				_parsedAsset = dir + file.file + (file.ext != null ? "." + file.ext : "");
 			}
 		}
 		return true;
@@ -154,27 +133,25 @@ class ModsFolderLibrary extends AssetLibrary implements IModsAssetLibrary {
 	}
 
 	function __listAppend(arr:Array<String>, folder:String) {
-		for(file in FileSystem.readDirectory('$basePath/$folder')) {
-			var fullPath = '$basePath/$folder/$file';
+		var fullFolderPath = Path.join([basePath, folder]);
+		if (!FileSystem.exists(fullFolderPath)) return;
+		
+		for(file in FileSystem.readDirectory(fullFolderPath)) {
+			var fullPath = Path.join([fullFolderPath, file]);
 			if (FileSystem.isDirectory(fullPath))
-				__listAppend(arr, '$folder$file/');
+				__listAppend(arr, folder + file + '/');
 			else
-				arr.push('$prefix$folder$file');
+				arr.push(prefix + folder + file);
 		}
 	}
 	#end
 
 	public function getAssetPath():String {
-     	var p = '$basePath/$_parsedAsset';
-    	trace("MOD ASSET PATH: " + p);
-	    return p;
+		var p = Path.normalize(Path.join([basePath, _parsedAsset]));
+		return p;
 	}
 
 	@:noCompletion public var folderPath(get, set):String;
-	@:noCompletion private inline function get_folderPath():String {
-		return basePath;
-	}
-	@:noCompletion private inline function set_folderPath(value:String):String {
-		return basePath = value;
-	}
+	@:noCompletion private inline function get_folderPath():String { return basePath; }
+	@:noCompletion private inline function set_folderPath(value:String):String { return basePath = value; }
 }
