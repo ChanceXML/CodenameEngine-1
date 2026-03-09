e4cc0cf563f8627dedc33a171129a890f4b22ed1
ChanceXML <joseguest1337@gmail.com>
2026-03-08 19:50:10 -0700

    Update MobileControls.hx

--- source/mobile/controls/MobileControls.hx
+++ source/mobile/controls/MobileControls.hx
@@ -4,7 +4,7 @@ import flixel.FlxG;
 import flixel.group.FlxGroup;
 import flixel.ui.FlxButton;
 import flixel.input.keyboard.FlxKey;
-import flixel.input.keyboard.FlxKeyboard;
+import flixel.input.FlxInput;
 
 class MobileControls extends FlxGroup
 {
@@ -25,24 +25,38 @@ class MobileControls extends FlxGroup
 		createActions(actions);
 	}
 
+	function pressKey(key:FlxKey, down:Bool)
+	{
+		var input:FlxInput<FlxKey> = FlxG.keys.get(key);
+
+		if (input == null) return;
+
+		if (down)
+			input.press();
+		else
+			input.release();
+	}
+
 	function createButton(x:Float, y:Float, img:String, key:FlxKey)
 	{
 		var btn = new FlxButton(x, y);
 		btn.loadGraphic("assets/images/mobile/buttons/" + img + ".png");
 
-		btn.alpha = 0.5;
+		btn.alpha = 0.6;
 
 		btn.onDown.callback = function()
 		{
-			btn.alpha = 0.9;
-			triggerKey(key, true);
+			btn.alpha = 1;
+			pressKey(key, true);
 		};
 
 		btn.onUp.callback = function()
 		{
-			btn.alpha = 0.5;
-			triggerKey(key, false);
+			btn.alpha = 0.6;
+			pressKey(key, false);
 		};
+
+		btn.onOut.callback = btn.onUp.callback;
 
 		add(btn);
 	}
@@ -52,17 +66,14 @@ class MobileControls extends FlxGroup
 		switch(type)
 		{
 			case UP_DOWN:
-
 				createButton(80, FlxG.height - 260, "UP", FlxKey.UP);
 				createButton(80, FlxG.height - 140, "DOWN", FlxKey.DOWN);
 
 			case LEFT_RIGHT:
-
 				createButton(20, FlxG.height - 180, "LEFT", FlxKey.LEFT);
 				createButton(140, FlxG.height - 180, "RIGHT", FlxKey.RIGHT);
 
 			case FULL:
-
 				createButton(80, FlxG.height - 260, "UP", FlxKey.UP);
 				createButton(20, FlxG.height - 180, "LEFT", FlxKey.LEFT);
 				createButton(140, FlxG.height - 180, "RIGHT", FlxKey.RIGHT);
@@ -77,24 +88,14 @@ class MobileControls extends FlxGroup
 			case NONE:
 
 			case A_B:
-
 				createButton(FlxG.width - 200, FlxG.height - 160, "A", FlxKey.ENTER);
 				createButton(FlxG.width - 100, FlxG.height - 160, "B", FlxKey.BACKSPACE);
 
 			case A_B_X_Y:
-
 				createButton(FlxG.width - 200, FlxG.height - 260, "Y", FlxKey.TAB);
 				createButton(FlxG.width - 100, FlxG.height - 260, "X", FlxKey.SEVEN);
 				createButton(FlxG.width - 200, FlxG.height - 140, "A", FlxKey.ENTER);
 				createButton(FlxG.width - 100, FlxG.height - 140, "B", FlxKey.BACKSPACE);
 		}
 	}
-
-	function triggerKey(key:FlxKey, pressed:Bool)
-{
-	if (pressed)
-		FlxG.keys._keyListMap[key] = true;
-	else
-		FlxG.keys._keyListMap[key] = false;
-}
-}
+		}
