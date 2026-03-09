4aaaf090a4c338041ee8e73332824943632952ea
ChanceXML <joseguest1337@gmail.com>
2026-03-09 14:11:06 -0600

    Update MobileControls.hx

--- source/mobile/controls/MobileControls.hx
+++ source/mobile/controls/MobileControls.hx
@@ -33,66 +33,75 @@ class MobileControls extends FlxGroup
 
 		btn.alpha = 0.5;
 
+		var isPressed:Bool = false;
+
 		btn.onDown.callback = function()
 		{
+			if (isPressed) return;
+			isPressed = true;
+
 			btn.alpha = 0.9;
 			triggerKey(key, true);
 		};
 
 		btn.onUp.callback = function()
 		{
+			if (!isPressed) return;
+			isPressed = false;
+
 			btn.alpha = 0.5;
 			triggerKey(key, false);
 		};
         
-        btn.onOut.callback = function()
-        {
-            if (btn.alpha == 0.9)
-            {
-                btn.alpha = 0.5;
-                triggerKey(key, false);
-            }
-        };
+		btn.onOut.callback = function()
+		{
+			if (isPressed)
+			{
+				isPressed = false;
+				btn.alpha = 0.5;
+				triggerKey(key, false);
+			}
+		};
 
 		add(btn);
 	}
 
 	function createDpad(type:Int)
-{
-	switch(type)
 	{
-		case UP_DOWN:
-			createButton(-545, 135, "UP", FlxKey.UP);
-			createButton(-545, 285, "DOWN", FlxKey.DOWN);
+		switch(type)
+		{
+			case UP_DOWN:
+				createButton(-545, 135, "UP", FlxKey.UP);
+				createButton(-545, 285, "DOWN", FlxKey.DOWN);
 
-		case LEFT_RIGHT:
-			createButton(-545, 285, "LEFT", FlxKey.LEFT);
-			createButton(-400, 285, "RIGHT", FlxKey.RIGHT);
+			case LEFT_RIGHT:
+				createButton(-545, 285, "LEFT", FlxKey.LEFT);
+				createButton(-400, 285, "RIGHT", FlxKey.RIGHT);
 
-		case FULL:
-			createButton(-440, 60, "UP", FlxKey.UP);
-			createButton(-555, 155, "LEFT", FlxKey.LEFT);
-			createButton(-320, 155, "RIGHT", FlxKey.RIGHT);
-			createButton(-440, 295, "DOWN", FlxKey.DOWN);
+			case FULL:
+				createButton(-440, 60, "UP", FlxKey.UP);
+				createButton(-555, 155, "LEFT", FlxKey.LEFT);
+				createButton(-320, 155, "RIGHT", FlxKey.RIGHT);
+				createButton(-440, 295, "DOWN", FlxKey.DOWN);
+		}
 	}
-}
 
-function createActions(type:Int)
-{
-	switch(type)
+	function createActions(type:Int)
 	{
-		case NONE:
+		switch(type)
+		{
+			case NONE:
 
-		case A_B:
-			createButton(375, 285, "A", FlxKey.ENTER);
-			createButton(525, 285, "B", FlxKey.BACKSPACE);
+			case A_B:
+				createButton(375, 285, "A", FlxKey.ENTER);
+				createButton(525, 285, "B", FlxKey.BACKSPACE);
 
-		case A_B_X_Y:
-			createButton(375, 135, "Y", FlxKey.TAB);
-			createButton(525, 135, "X", FlxKey.SEVEN);
-			createButton(375, 285, "A", FlxKey.ENTER);
-			createButton(525, 285, "B", FlxKey.BACKSPACE);
-	}
+			case A_B_X_Y:
+				createButton(375, 135, "Y", FlxKey.TAB);
+				createButton(525, 135, "X", FlxKey.SEVEN);
+				createButton(375, 285, "A", FlxKey.ENTER);
+				createButton(525, 285, "B", FlxKey.BACKSPACE);
+		}
 	}
 
 	function triggerKey(key:FlxKey, pressed:Bool)
@@ -102,4 +111,3 @@ function createActions(type:Int)
 		Lib.current.stage.dispatchEvent(fakeEvent);
 	}
 		}
-		
