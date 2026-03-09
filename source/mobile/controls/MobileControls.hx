d15049bdd5639e2f84ed4372f5ac848c78f49fc7
ChanceXML <joseguest1337@gmail.com>
2026-03-08 19:23:54 -0700

    revamp

--- source/mobile/controls/MobileControls.hx
+++ source/mobile/controls/MobileControls.hx
@@ -7,8 +7,11 @@ import flixel.FlxCamera;
 
 class MobileControls extends FlxGroup
 {
+	public static inline var NONE:Int = -1;
+
 	public static inline var UP_DOWN:Int = 0;
-	public static inline var FULL:Int = 1;
+	public static inline var LEFT_RIGHT:Int = 1;
+	public static inline var FULL:Int = 2;
 
 	public static inline var A_B:Int = 0;
 	public static inline var A_B_X_Y:Int = 1;
@@ -24,13 +27,14 @@ class MobileControls extends FlxGroup
 		createActions(actions);
 	}
 
-	function createButton(x:Float,y:Float,img:String,key:Int)
+	function createButton(x:Float, y:Float, img:String, key:Int)
 	{
-		var btn = new FlxSprite(x,y);
+		var btn = new FlxSprite(x, y);
 		btn.loadGraphic("assets/images/mobile/buttons/" + img + ".png");
 
 		btn.scrollFactor.set();
 		btn.cameras = [cam];
+
 		btn.alpha = 0.5;
 
 		btn.update = function(elapsed:Float)
@@ -55,14 +59,66 @@ class MobileControls extends FlxGroup
 		switch(type)
 		{
 			case UP_DOWN:
-				createButton(80,FlxG.height-220,"UP",FlxG.keys.UP);
-				createButton(80,FlxG.height-110,"DOWN",FlxG.keys.DOWN);
+
+				createButton(
+					80,
+					FlxG.height - 260,
+					"UP",
+					FlxG.keys.UP
+				);
+
+				createButton(
+					80,
+					FlxG.height - 140,
+					"DOWN",
+					FlxG.keys.DOWN
+				);
+
+			case LEFT_RIGHT:
+
+				createButton(
+					20,
+					FlxG.height - 180,
+					"LEFT",
+					FlxG.keys.LEFT
+				);
+
+				createButton(
+					140,
+					FlxG.height - 180,
+					"RIGHT",
+					FlxG.keys.RIGHT
+				);
 
 			case FULL:
-				createButton(80,FlxG.height-220,"UP",FlxG.keys.UP);
-				createButton(0,FlxG.height-160,"LEFT",FlxG.keys.LEFT);
-				createButton(160,FlxG.height-160,"RIGHT",FlxG.keys.RIGHT);
-				createButton(80,FlxG.height-110,"DOWN",FlxG.keys.DOWN);
+
+				createButton(
+					80,
+					FlxG.height - 260,
+					"UP",
+					FlxG.keys.UP
+				);
+
+				createButton(
+					20,
+					FlxG.height - 180,
+					"LEFT",
+					FlxG.keys.LEFT
+				);
+
+				createButton(
+					140,
+					FlxG.height - 180,
+					"RIGHT",
+					FlxG.keys.RIGHT
+				);
+
+				createButton(
+					80,
+					FlxG.height - 100,
+					"DOWN",
+					FlxG.keys.DOWN
+				);
 		}
 	}
 
@@ -70,15 +126,53 @@ class MobileControls extends FlxGroup
 	{
 		switch(type)
 		{
+			case NONE:
+
 			case A_B:
-				createButton(FlxG.width-200,FlxG.height-160,"A",FlxG.keys.ENTER);
-				createButton(FlxG.width-100,FlxG.height-160,"B",FlxG.keys.BACKSPACE);
+
+				createButton(
+					FlxG.width - 200,
+					FlxG.height - 160,
+					"A",
+					FlxG.keys.ENTER
+				);
+
+				createButton(
+					FlxG.width - 100,
+					FlxG.height - 160,
+					"B",
+					FlxG.keys.BACKSPACE
+				);
 
 			case A_B_X_Y:
-				createButton(FlxG.width-200,FlxG.height-220,"Y",FlxG.keys.TAB);
-				createButton(FlxG.width-100,FlxG.height-220,"X",FlxG.keys.SEVEN);
-				createButton(FlxG.width-200,FlxG.height-110,"A",FlxG.keys.ENTER);
-				createButton(FlxG.width-100,FlxG.height-110,"B",FlxG.keys.BACKSPACE);
+
+				createButton(
+					FlxG.width - 200,
+					FlxG.height - 260,
+					"Y",
+					FlxG.keys.TAB
+				);
+
+				createButton(
+					FlxG.width - 100,
+					FlxG.height - 260,
+					"X",
+					FlxG.keys.SEVEN
+				);
+
+				createButton(
+					FlxG.width - 200,
+					FlxG.height - 140,
+					"A",
+					FlxG.keys.ENTER
+				);
+
+				createButton(
+					FlxG.width - 100,
+					FlxG.height - 140,
+					"B",
+					FlxG.keys.BACKSPACE
+				);
 		}
 	}
-}
+		}
