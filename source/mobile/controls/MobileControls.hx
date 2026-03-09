ff5030af596bd87ec5867b7abfa8807708541ba3
ChanceXML <joseguest1337@gmail.com>
2026-03-09 16:48:12 -0700

    Update MobileControls.hx

--- source/mobile/controls/MobileControls.hx
+++ source/mobile/controls/MobileControls.hx
@@ -1,117 +1,110 @@
 package mobile.controls;
 
 import flixel.FlxG;
+import flixel.FlxCamera;
 import flixel.group.FlxGroup;
 import flixel.ui.FlxButton;
-import flixel.FlxCamera;
 import flixel.input.keyboard.FlxKey;
-import openfl.events.KeyboardEvent;
 import openfl.Lib;
 
 class MobileControls extends FlxGroup
 {
-    public var buttonCam:FlxCamera;
+	public static inline var NONE:Int = -1;
+	public static inline var UP_DOWN:Int = 0;
+	public static inline var LEFT_RIGHT:Int = 1;
+	public static inline var FULL:Int = 2;
+	public static inline var A_B:Int = 0;
+	public static inline var A_B_X_Y:Int = 1;
 
-    public static inline var NONE:Int = -1;
-    public static inline var UP_DOWN:Int = 0;
-    public static inline var LEFT_RIGHT:Int = 1;
-    public static inline var FULL:Int = 2;
+	public var buttonCam:FlxCamera;
 
-    public static inline var A_B:Int = 0;
-    public static inline var A_B_X_Y:Int = 1;
+	public function new(dpad:Int, actions:Int)
+	{
+		super();
+		buttonCam = new FlxCamera(0, 0, 1280, 720);
+		buttonCam.scroll.set(0, 0);
+		buttonCam.bgColor = 0;
+		for(btn in members)
+			btn.cameras = [buttonCam];
+		createDpad(dpad);
+		createActions(actions);
+	}
 
-    public function new(dpad:Int, actions:Int)
-    {
-        super();
+	function createButton(x:Float, y:Float, img:String, key:FlxKey)
+	{
+		var btn = new FlxButton(x, y);
+		btn.loadGraphic("assets/images/mobile/buttons/" + img + ".png");
+		btn.alpha = 0.5;
+		btn.cameras = [buttonCam];
+		var isPressed:Bool = false;
 
-        buttonCam = new FlxCamera(0, 0, FlxG.width, FlxG.height);
-        buttonCam.scroll.set(0,0);
-        buttonCam.bgColor = 0x00000000;
+		btn.onDown.callback = function() {
+			if(isPressed) return;
+			isPressed = true;
+			btn.alpha = 0.9;
+			triggerKey(key, true);
+		};
 
-        if(!FlxG.cameras.list.contains(buttonCam))
-            FlxG.cameras.add(buttonCam);
+		btn.onUp.callback = function() {
+			if(!isPressed) return;
+			isPressed = false;
+			btn.alpha = 0.5;
+			triggerKey(key, false);
+		};
 
-        FlxCamera.defaultCameras = [FlxG.camera];
+		btn.onOut.callback = function() {
+			if(!isPressed) return;
+			isPressed = false;
+			btn.alpha = 0.5;
+			triggerKey(key, false);
+		};
 
-        createDpad(dpad);
-        createActions(actions);
-    }
+		add(btn);
+	}
 
-    function createButton(x:Float, y:Float, img:String, key:Int)
-    {
-        var btn = new FlxButton(x, y);
-        btn.loadGraphic("assets/images/mobile/buttons/" + img + ".png");
-        btn.alpha = 0.5;
+	function createDpad(type:Int)
+	{
+		switch(type)
+		{
+			case UP_DOWN:
+				createButton(32.5, 432.5, "UP", FlxKey.UP);
+				createButton(32.5, 582.5, "DOWN", FlxKey.DOWN);
 
-        btn.cameras = [buttonCam];
+			case LEFT_RIGHT:
+				createButton(32.5, 582.5, "LEFT", FlxKey.LEFT);
+				createButton(177.5, 582.5, "RIGHT", FlxKey.RIGHT);
 
-        var isPressed:Bool = false;
-        btn.onDown.callback = function()
-        {
-            if(isPressed) return;
-            isPressed = true;
-            btn.alpha = 0.9;
-            triggerKey(key, true);
-            triggerKey(key, false);
-        };
+			case FULL:
+				createButton(137.5, 387.5, "UP", FlxKey.UP);
+				createButton(22.5, 482.5, "LEFT", FlxKey.LEFT);
+				createButton(257.5, 482.5, "RIGHT", FlxKey.RIGHT);
+				createButton(137.5, 592.5, "DOWN", FlxKey.DOWN);
+		}
+	}
 
-        btn.onUp.callback = function()
-        {
-            isPressed = false;
-            btn.alpha = 0.5;
-        };
+	function createActions(type:Int)
+	{
+		switch(type)
+		{
+			case NONE:
 
-        btn.onOut.callback = function()
-        {
-            isPressed = false;
-            btn.alpha = 0.5;
-        };
+			case A_B:
+				createButton(982.5, 582.5, "A", FlxKey.ENTER);
+				createButton(1132.5, 582.5, "B", FlxKey.BACKSPACE);
 
-        add(btn);
-    }
+			case A_B_X_Y:
+				createButton(982.5, 432.5, "Y", FlxKey.TAB);
+				createButton(1135.5, 432.5, "X", FlxKey.SEVEN);
+				createButton(982.5, 582.5, "A", FlxKey.ENTER);
+				createButton(1132.5, 582.5, "B", FlxKey.BACKSPACE);
+		}
+	}
 
-    function createDpad(type:Int)
-    {
-        switch(type)
-        {
-            case UP_DOWN:
-                createButton(-545, 135, "UP", FlxKey.UP);
-                createButton(-545, 285, "DOWN", FlxKey.DOWN);
-
-            case LEFT_RIGHT:
-                createButton(-545, 285, "LEFT", FlxKey.LEFT);
-                createButton(-400, 285, "RIGHT", FlxKey.RIGHT);
-
-            case FULL:
-                createButton(-440, 60, "UP", FlxKey.UP);
-                createButton(-555, 155, "LEFT", FlxKey.LEFT);
-                createButton(-320, 155, "RIGHT", FlxKey.RIGHT);
-                createButton(-440, 295, "DOWN", FlxKey.DOWN);
-        }
-    }
-
-    function createActions(type:Int)
-    {
-        switch(type)
-        {
-            case NONE:
-
-            case A_B:
-                createButton(375, 285, "A", FlxKey.ENTER);
-                createButton(525, 285, "B", FlxKey.BACKSPACE);
-
-            case A_B_X_Y:
-                createButton(375, 135, "Y", FlxKey.TAB);
-                createButton(525, 135, "X", FlxKey.SEVEN);
-                createButton(375, 285, "A", FlxKey.ENTER);
-                createButton(525, 285, "B", FlxKey.BACKSPACE);
-        }
-    }
-
-    function triggerKey(key:Int, pressed:Bool)
-    {
-        var eventType = pressed ? KeyboardEvent.KEY_DOWN : KeyboardEvent.KEY_UP;
-        var fakeEvent = new KeyboardEvent(eventType, true, false, 0, key);
-        Lib.current.stage.dispatchEvent(fakeEvent);
-    }
+	function triggerKey(key:FlxKey, pressed:Bool)
+	{
+		if(pressed)
+			FlxG.keys.justPressed.set(key, true);
+		else
+			FlxG.keys.justReleased.set(key, true);
+	}
 		}
