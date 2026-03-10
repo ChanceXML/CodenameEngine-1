bbed5b1a7d74e1663b4ea36e9c320fae9f9a11fb
ChanceXML <joseguest1337@gmail.com>
2026-03-09 20:34:58 -0700

    testing

--- source/mobile/MobilePermissions.hx
+++ source/mobile/MobilePermissions.hx
@@ -1,31 +1,8 @@
 package mobile;
 
-#if android
-import extension.androidtools.os.Build.VERSION as AndroidVersion;
-import extension.androidtools.os.Build.VERSION_CODES as AndroidVersionCode;
-import extension.androidtools.Permissions as AndroidPermissions;
-#end
-
 class StorageUtil
 {
-	#if android
 	public static function requestPermissions():Void
 	{
-		if (AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU)
-		{
-			AndroidPermissions.requestPermissions([
-				"android.permission.READ_MEDIA_IMAGES",
-				"android.permission.READ_MEDIA_VIDEO",
-				"android.permission.READ_MEDIA_AUDIO"
-			]);
-		}
-		else
-		{
-			AndroidPermissions.requestPermissions([
-				"android.permission.READ_EXTERNAL_STORAGE",
-				"android.permission.WRITE_EXTERNAL_STORAGE"
-			]);
-		}
 	}
-	#end
 }
