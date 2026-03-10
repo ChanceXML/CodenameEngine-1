1f31bf82c2071cee2293de27f5d3383a6cdd7ff2
ChanceXML <joseguest1337@gmail.com>
2026-03-09 18:29:00 -0700

    Update FreeplayState.hx

--- source/funkin/menus/FreeplayState.hx
+++ source/funkin/menus/FreeplayState.hx
@@ -117,11 +117,10 @@ class FreeplayState extends MusicBeatState
 	 */
 	public var interpColor:FlxInterpolateColor;
 
-	function addMobile(dpad:Int, actions:Int)
-{
-    var buttonCam = new FlxCamera(0, 0, 1280, 720);
-    FlxG.cameras.add(buttonCam);
-    add(new MobileControls(dpad, actions, buttonCam));
+	function addMobile(dpad:Int, actions:Int, cam:FlxCamera)
+    {
+        add(new MobileControls(dpad, actions, cam));
+    }
 }
 	
 	override function create()
@@ -159,7 +158,11 @@ class FreeplayState extends MusicBeatState
 		grpSongs = new FlxTypedGroup<Alphabet>();
 		add(grpSongs);
 
-        addMobile(FULL, A_B_X_Y);
+        buttonCam = new FlxCamera(0, 0, 1280, 720);
+        buttonCam.scroll.set(0, 0);
+        FlxG.cameras.add(buttonCam);
+
+        addMobile(FULL, A_B_X_Y, buttonCam);
 
 		for (i in 0...songs.length)
 		{
